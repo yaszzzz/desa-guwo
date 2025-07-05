bf12f662bb280b2556615d7bbd2113561c707eb9
yaszzzz <fightergamerz77@gmail.com>
2025-07-05 15:10:24 +0700

    updated34

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 const images = [
   '/assets/photos/hero/foto3.jpg',
   '/assets/photos/hero/foto4.jpg',
-  '/public/assets/photos/letak/bodeh.jpg',
+  '/assets/photos/letak/bodeh.jpg',
   // tambahkan foto lain di sini
 ]
 
