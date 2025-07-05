102393ca5987b3338ea3b358faa321d636cba5dc
yaszzzz <fightergamerz77@gmail.com>
2025-07-05 15:07:41 +0700

    updatedd

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 const images = [
   '/assets/photos/hero/foto3.jpg',
   '/assets/photos/hero/foto4.jpg',
-  '/assets/photos/letak/balai-desa.jpg',
+  '/public/assets/photos/letak/bodeh.jpg',
   // tambahkan foto lain di sini
 ]
 
